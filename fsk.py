23381cdd84e6cb237d7a858acbe5ef826afe96a7
Mikhail Yudin <fagci.nsk@gmail.com>
2022-03-03 02:49:20 +0700

    refactor: cleanup

--- fsk.py
+++ fsk.py
@@ -1,44 +1,39 @@
 #!/usr/bin/env python3
 
-from numpy import arange, argmax, int16, pi as PI, sin, array_split
+from numpy import arange, argmax, array_split, int16, pi as PI, sin
 from numpy.core.multiarray import concatenate
 from numpy.fft import fft, fftfreq
 from scipy.io.wavfile import write
 
+message = str(arange.__doc__)
+
 SR = 44100
 F0 = 440
-
-SYM_RATE = 8 # TODO: make faster version work
+SYM_RATE = 8
+FDEV = 16
 
 PI2 = 2 * PI
 
-FMUL = 16
-
 TPS = int(SR / SYM_RATE)
-
-message = '0Z'
-message = 'NumPy is the fundamental package for scientific computing in Python'
+MSG_LEN = len(message)
 
 print('Source message:', message)
 
-MSG_LEN = len(message)
-
-t = arange(0, TPS)
 
 print('Encoding...')
 
-freqs = [F0 + ord(c) * FMUL for c in message]
+freqs = [F0 + ord(c) * FDEV for c in message]
 print(freqs)
 
-modulated = concatenate([
-    sin(PI2 * t / SR * f)
-    for f in freqs
-])
+t = arange(TPS)
+modulated = concatenate([sin(PI2 * t / SR * f) for f in freqs])
+
+print()
 
 print('Decoding...')
 
 T = modulated.size // TPS
-ff = fftfreq(TPS, 1/SR)
+ff = fftfreq(TPS, 1 / SR)
 
 freqs = [
     int(abs(ff[argmax(abs(fft(carrier)))]))
@@ -47,7 +42,7 @@ freqs = [
 
 print(freqs)
 
-decoded = ''.join(chr(int((f - F0) / FMUL)) for f in freqs)
+decoded = ''.join(chr((f - F0) // FDEV) for f in freqs)
 
 print('Decoded message:', decoded)
 
