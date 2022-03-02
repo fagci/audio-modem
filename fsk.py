2c567f2363978e5d8aac2ba22fbeb129c468cfad
Mikhail Yudin <fagci.nsk@gmail.com>
2022-03-03 03:43:20 +0700

    refactor

--- fsk.py
+++ fsk.py
@@ -1,49 +1,67 @@
 #!/usr/bin/env python3
 
+from zlib import compress, decompress
+
 from numpy import arange, argmax, array_split, int16, pi as PI, sin
 from numpy.core.multiarray import concatenate
 from numpy.fft import fft, fftfreq
 from scipy.io.wavfile import write
 
-message = str(arange.__doc__)
+message = str(argmax.__doc__)
 
 SR = 44100
-F0 = 440
-SYM_RATE = 8
-FDEV = 16
+F0 = 100
+SYM_RATE = 10
+FDEV = 10
 
 PI2 = 2 * PI
 
 TPS = int(SR / SYM_RATE)
-MSG_LEN = len(message)
-
-print('Source message:', message)
 
 
-print('Encoding...')
+def c2f(c):
+    return F0 + c * FDEV
 
-freqs = [F0 + ord(c) * FDEV for c in message]
-print(freqs)
 
-t = arange(TPS)
-modulated = concatenate([sin(PI2 * t / SR * f) for f in freqs])
+def f2c(f):
+    return (f - F0) // FDEV
 
-print()
 
-print('Decoding...')
+def encode(msg):
+    t = arange(TPS)
+    parts = [sin(PI2 * t / SR * c2f(c)) for c in msg]
+    return concatenate(parts)
 
-T = modulated.size // TPS
-ff = fftfreq(TPS, 1 / SR)
 
-freqs = [
-    int(abs(ff[argmax(abs(fft(carrier)))]))
-    for carrier in array_split(modulated, T)
-]
+def decode(signal):
+    T = signal.size // TPS
+    ff = fftfreq(TPS, 1 / SR)
 
-print(freqs)
+    freqs = [
+        int(abs(ff[argmax(abs(fft(carrier)))]))
+        for carrier in array_split(signal, T)
+    ]
 
-decoded = ''.join(chr((f - F0) // FDEV) for f in freqs)
+    return bytes(f2c(f) for f in freqs)
 
-print('Decoded message:', decoded)
 
-write('fsk.wav', SR, int16(modulated * 32767))
+def main():
+    print('Source message:', message)
+
+    print('Encoding...')
+    compressed = compress(message.encode())
+    encoded = encode(compressed)
+
+    print('Decoding...')
+    compressed = decode(encoded)
+    decoded = decompress(compressed).decode()
+
+    print('Decoded message:', decoded)
+
+    write('fsk.wav', SR, int16(encoded * 32767))
+
+    assert (decoded == message)
+
+
+if __name__ == '__main__':
+    main()
